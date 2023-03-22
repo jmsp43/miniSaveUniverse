74f7c85cacda3d189ad4c9bdadca882305cf181b
Jenna Perez <jmsp43@gmail.com>
2023-03-22 11:33:27 -0400

    fleshing out classes

--- script.js
+++ script.js
@@ -1,90 +1,58 @@
-//Needs:
-//Class of USS ship (child of shipFactory) with extra retreat method
-
-class UssShip {
-    constructor() {
-        
-    }
-    retreat() {
-        
-    }
-}
-
-
-class shipFactory {
-    constructor(hull, firepower, accuracy) {
-        this.hull = hull
-        this.firepower = firepower
-        this. accuracy = accuracy
-    }
-    pewPew() {
-        
-    }
-    getShot() {
-        hull--
-        
-        
-    }
-    makeShot() {
-        
-    }
-}
-
-
-//Factory for ships with required properties of hull, firepower, and accuracy, and methods that both alien and uss ships use
-
-class AlienShip {
-    constructor(){
-    //do i need a constructor here? will there be additional properties that an alien ship has that the USS ship doesn't?
-    }
-    //what additional methods do i need?
-}
-
-
+////////////   PLAN   //////////////
 
 //kind of like google chrome dino game except instead of jumping, you pew pew
 //and the ships are coming towards you mad slow in a single file line
-//USS pew pew:
-    //misses: aliens attack and hull loses defense
+
+//USS shoots:
+    //misses: aliens attack
+        //if they land the shot: USS hull loses defense, their firepower increases
+        //else if they miss: USS pew pews at them again, cycle restarts
     //lands: aliens hull loses defense, USS firepower increase
-    //if 1 Alien ship destroyed, can either attack next ship or retreat
-        //retreat, game over
-        //if defeated all aliens, you win game over
-//Aliens pew pew:
-    //misses: USS attacks and aliens hull loses defense
+        //if Alien ship hull gets to 0, player decided to either attack next ship or retreat
+            //retreat, game over
+            //if defeated all aliens, you win, game over
+//Aliens shoots:
+    //misses: USS attacks and aliens hull loses defense, USS firepower increases
     //lands: USS hull loses defense, alien firepower increase
     //if USS hull gets to 0, you lose game over
 
 
 
 
+//Game round goes like this:
+
+// USS attacks the first alien ship
+    // If the ship survives, it attacks USS
+        // If USS survive, USS attack the ship again
+            // If it survives, it attacks USS again ... etc
+    // If USS destroy the ship, USS either attacks next ship or retreats
+        // If retreats, the game is over
 
 
+// You win the game if USS destroy all of the aliens
+// You lose the game if USS is destroyed
 
 
+//////////////////////////////////////////////////////////////
 
 
-// A game round would look like this:
-// You attack the first alien ship
-// If the ship survives, it attacks you
-// If you survive, you attack the ship again
-// If it survives, it attacks you again ... etc
-// If you destroy the ship, you have the option to attack the next ship or to retreat
-// If you retreat, the game is over, perhaps leaving the game open for further developments or options
-// You win the game if you destroy all of the aliens
-// You lose the game if you are destroyed
 
 
 
 // Ship Properties
+
 // hull is the same as hitpoints. If hull reaches 0or less, the ship is destroyed
+
 // firepower is the amount of damage done to the hull of the target with a successful hit
+
 // accuracy is the chance between 0 and 1 that the ship will hit its target
 // Your spaceship, the USS Assembly should have the following properties:
 
 // hull - 20
 // firepower - 5
 // accuracy - .7
+
+
 // The alien ships should each have the following ranged properties determined randomly:
 
 // hull - between 3and 6
@@ -120,3 +88,60 @@ class AlienShip {
 // A good rule of thumb is start with the actors and then the actions. What actors do we need? In this case, we need our spaceship and the alien spaceships. An action these ships can take is to attack something. Perhaps a ship object (an actor) could therefore have an attack method (an action).
 
 // A repeating action in the game is that these ships attack each other until one of them has been destroyed. This might necessitate a loop or multiple loops.
+
+
+
+
+//Factory for all ships with required properties of hull, firepower, accuracy, and shipType. includes methods that both alien and uss ships use
+class ShipFactory {
+    constructor(hull, firepower, accuracy, shipType) {
+        this.hull = hull
+        this.firepower = firepower
+        this.accuracy = accuracy
+        this.shipType = shipType
+    }
+    pewPew() {
+        //create visual of laser going toward enemy
+        //if location of shot === location of enemy, go to makeShot()
+    }
+    getShot() {
+        //hull = hull - firepower of player who shot you
+        if (hull <= 0 && shipType === 'uss') {
+            //USS ship is destroyed
+            console.log('game over')
+        }       
+        else if (hull <= 0 && shipType === 'enemy') {
+            //enemy ship is destroyed
+            //if there are more enemy ships:
+                //give player option to retreat or keep playing
+        }
+    }
+    makeShot() {
+    }
+}
+
+
+
+//Class of USS ship (child of shipFactory) with extra retreat method
+
+class UssShip extends ShipFactory {
+    constructor() {
+        //are there any properties that USS has that enemy doesn't?
+    }
+    retreat() {
+        //show USS hovering away to offscreen
+        console.log('game over')
+    }
+}
+
+
+
+class AlienShip extends ShipFactory{
+    constructor(){
+    //do i need a constructor here? will there be additional properties that an alien ship has that the USS ship doesn't?
+    }
+    movingTowardUSS() {
+        //continuously inching toward USS
+    }
+}
+
